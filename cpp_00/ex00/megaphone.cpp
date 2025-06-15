0db562613a54afa9edd120eeb4b439ea03af684d
Elif Ebrar Kavak <elikavak@k1m41s04.42istanbul.com.tr>
2025-06-15 12:05:35 +0300

    ex00changes

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: elikavak <elikavak@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 13:27:00 by elikavak          #+#    #+#             */
-/*   Updated: 2025/06/13 14:27:49 by elikavak         ###   ########.fr       */
+/*   Updated: 2025/06/15 12:04:33 by elikavak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,10 @@ int main(int argc, char **argv)
 	for (int i = 1; i < argc; i++)
 	{
 		for (size_t j = 0; j < strlen(argv[i]); j++)
-		{
-			if (isalpha(argv[i][j]) && islower(argv[i][j]))
-				argv[i][j] = toupper(argv[i][j]);
-		}
+			argv[i][j] = toupper(argv[i][j]);
 		std::cout<<argv[i];
+		if (i != argc -1)
+			std::cout << " ";
 	}
 	std::cout<<std::endl;
 	return (0);
